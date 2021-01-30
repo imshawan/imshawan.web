b6077c8e1d1822135200086b7f4ebcc5ae816355
Shawan Mandal <70664409+imshawan@users.noreply.github.com>
2021-01-30 20:18:05 +0530

    Update validate.js

--- assets/vendor/php-email-form/validate.js
+++ assets/vendor/php-email-form/validate.js
@@ -89,7 +89,6 @@ jQuery(document).ready(function($) {
         i.next('.validate').html((ierror ? (i.attr('data-msg') != undefined ? i.attr('data-msg') : 'wrong Input') : '')).show('blind');
       }
     });
-    return false;
   });
 
 });
