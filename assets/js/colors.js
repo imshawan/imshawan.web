072929fdcfcb676688a23688f76f8a7cf35b4067
Shawan Mandal <70664409+imshawan@users.noreply.github.com>
2021-05-02 21:43:30 +0530

    Applied gradient

--- assets/js/colors.js
+++ assets/js/colors.js
@@ -3,13 +3,19 @@
         function changeColor() {
             // Array containing colors
             var colors = [
-                '255, 0, 0', '0, 255, 0', '0, 0, 255', '255, 166, 0', '0, 0, 0', '82, 5, 123', '22, 36, 71', '84, 18, 59',
-                '68, 55, 55', '92, 84, 112', '157, 11, 40', '253, 202, 64', '40, 150, 114', '120, 104, 230', '28, 197, 220'
+                '255, 0, 0', '0, 255, 0', '0, 0, 255', '255, 166, 0', '0, 0, 0', '82, 5, 123', '84, 18, 59',
+                '92, 84, 112', '157, 11, 40', '253, 202, 64', '40, 150, 114', '120, 104, 230', '28, 197, 220'
             ];
-              
+            var gradientColors = ['255, 0, 157', '255, 255, 0', '0, 255, 238'];
+
             // selecting random color
             var random_color = colors[Math.floor(
                     Math.random() * colors.length)];
             document.documentElement.style
             .setProperty('--colors', random_color);  
+
+            var random_Gradcolor = gradientColors[Math.floor(
+                Math.random() * gradientColors.length)];
+            document.documentElement.style
+            .setProperty('--gradientCol', random_Gradcolor);
         } 
