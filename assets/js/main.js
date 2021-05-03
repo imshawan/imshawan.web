a6194cb54469f82779e5c8167c019a409b481d43
Shawan Mandal <70664409+imshawan@users.noreply.github.com>
2021-05-03 16:29:22 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,4 @@
-/**
-* Template Name: MyResume - v2.2.0
-* Template URL: https://bootstrapmade.com/free-html-bootstrap-template-my-resume/
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
-*/
+
 !(function($) {
   "use strict";
 
@@ -14,6 +9,24 @@
         $(this).remove();
       });
     }
+    // JavaScript code to pick a random color from array and injecting it to html
+    // Array containing colors
+    var colors = [
+      '255, 0, 0', '0, 255, 0', '0, 0, 255', '255, 166, 0', '0, 0, 0', '82, 5, 123', '84, 18, 59',
+      '92, 84, 112', '157, 11, 40', '253, 202, 64', '40, 150, 114', '120, 104, 230', '28, 197, 220'
+    ];
+    var gradientColors = ['255, 0, 157', '255, 255, 0', '0, 255, 238'];
+
+    // selecting random color
+    var random_color = colors[Math.floor(
+            Math.random() * colors.length)];
+    document.documentElement.style
+    .setProperty('--colors', random_color);  
+
+    var random_Gradcolor = gradientColors[Math.floor(
+        Math.random() * gradientColors.length)];
+    document.documentElement.style
+    .setProperty('--gradientCol', random_Gradcolor);
   });
 
   // Hero typed
