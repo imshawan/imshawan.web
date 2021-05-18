106828450a4330e4c5680f08bd464e629fce40c7
Shawan Mandal <70664409+imshawan@users.noreply.github.com>
2021-05-18 21:36:51 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,7 +12,7 @@
     // JavaScript code to pick a random color from array and injecting it to html
     // Array containing colors
     var colors = [
-      '255, 0, 0', '0, 255, 0', '0, 0, 255', '255, 166, 0', '82, 5, 123', '84, 18, 59',
+      '255, 0, 0', '0, 0, 255', '255, 166, 0', '82, 5, 123', '84, 18, 59',
       '157, 11, 40', '253, 202, 64', '40, 150, 114', '120, 104, 230', '28, 197, 220'
     ];
     var gradientColors = ['255, 0, 157', '255, 255, 0', '0, 255, 238'];
